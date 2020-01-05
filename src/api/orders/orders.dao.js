7de593c0346d8f4de84c947ce9570a292c7d465d
thaisamorandini89 <thaisamorandini@gmail.com>
2020-01-04 23:41:17 -0200

    Validou a quantidade de produtos disponiveis antes de salvar o pedido

--- src/api/orders/orders.dao.js
+++ src/api/orders/orders.dao.js
@@ -2,21 +2,55 @@ import { instances } from 'hapi-sequelizejs'
 import { getObjectOr404 } from '../utils/database.utils';
 
 const Order = instances.getModel('order');
+const Product = instances.getModel('product');
 
 export default class OrdersDAO {
 
-  async findAll(params) {
-    return Order.findAll();
+  async findAll(params) { 
+    return Order.findAll({
+      params,
+      include:[
+        'user',
+        {
+          model: this.Product,
+          as: 'products',
+          through: { attributes: ['quantity'] },
+          attributes: ['id', 'description', 'value'],
+        },
+      ],
+    });
   }
 
   async findByID(id) {
     return getObjectOr404(Order, {
-      where: { id }
+      where: { id },
+      include: [
+        'user',
+        {
+          model: this.Product,
+          as: 'products',
+          through: { attributes: ['quantity'] },
+          attributes: ['id', 'description', 'value'],
+        },
+      ],
     });
   }
 
   async create(data) {
-    return Order.create(data);
+    const { products } = data;
+    const order = await Order.create(data);
+
+    if(products){
+      for(const product of products){
+        const { id, quantity } = product;
+        await order.addProducts(id,{
+          through: {
+            quantity: quantity
+          }
+        });
+      }
+    }
+    return await this.findByID(pedido.id);
   }
 
   async update(id, data) {
