e16c5284c979c8c5a7360b40b57ad70d7a4e92dc
thaisamorandini89 <thaisamorandini@gmail.com>
2020-01-04 22:15:07 -0200

    filtro em products.dao.js

--- src/api/products/products.dao.js
+++ src/api/products/products.dao.js
@@ -1,11 +1,18 @@
 import { instances } from 'hapi-sequelizejs'
 import { getObjectOr404 } from '../utils/database.utils';
+import Sequelize from 'sequelize';
 
 const Product = instances.getModel('product');
+
+const operator = Sequelize.Op;
 
 export default class ProductsDAO {
 
   async findAll(params) {
+    
+    if(params.description){
+        params.description = { [op.like]: `%${params.description}%`};
+    }
     return Product.findAll({
       where: params,
       include: [ 'category' ]
